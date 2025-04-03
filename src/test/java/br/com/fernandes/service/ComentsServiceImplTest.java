1ac2e4965feee906233d05903b5745c3ca8558d5
GustavoSFer <gustavo.sfernander@gmail.com>
2025-04-02 22:07:53 -0300

    fix: melhorando o teste e mocando da forma correta

--- src/test/java/br/com/fernandes/service/ComentsServiceImplTest.java
+++ src/test/java/br/com/fernandes/service/ComentsServiceImplTest.java
@@ -11,6 +11,7 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,24 +25,29 @@ class ComentsServiceImplTest {
     @Mock
     ComentsClient client;
 
+    private List<ComentsResponse> mockComentsList;
+
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.openMocks(this);
+        ComentsResponse coment1 = new ComentsResponse(1, 1, "nome 1", "nome@gmail.com", "Coment 1");
+        ComentsResponse coment2 = new ComentsResponse(1, 2, "nome 2", "nome@gmail.com", "Coment 2");
+        mockComentsList = Arrays.asList(coment1, coment2);
     }
 
     @Test
-    @DisplayName("Se tiver um comentario espero que seja retornado uma lista com tamanho de 1")
+    @DisplayName("Deve retorna uma lista de comentarios quando tiver")
     void getComents() {
         // Given
-        ComentsResponse coment = new ComentsResponse(1,2, "nome 1", "nome@gmail.com", "body texto");
-        List<ComentsResponse> listResult = new ArrayList<>();
-        listResult.add(coment);
         // When
-        when(client.getComents()).thenReturn(listResult);
+        when(client.getComents()).thenReturn(mockComentsList);
+
+        List<ComentsResponse> listResult = comentsService.getComents();
         // Then
-        assertEquals(1, listResult.size());
-        assertEquals(2, coment.id());
-        assertNotNull(coment.email());
-        assertNotNull(coment.body());
+        assertEquals(2, listResult.size());
+        assertEquals(1, listResult.get(0).postId());
+        assertNotNull(listResult.get(0).body());
+        assertEquals("Coment 1", listResult.get(0).body());
+        assertEquals("Coment 2", listResult.get(1).body());
     }
 }
